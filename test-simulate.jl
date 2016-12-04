8c7b9490573956d876c9c52a83c11f95d01e5a13
7akase <yasuhide.takase@gmail.com>
2016-12-05 01:03:31 +0900

    add 2nd-derivative of cos-shaped step which doesn't work

--- test-simulate.jl
+++ test-simulate.jl
@@ -25,24 +25,29 @@ end
 # ---------------------------------------------------------------------- 
 function hpf1()
   t = [0:0.1:10.0;]
-  noms = [1.0, 10.0]
-  dens = [1.0, 0.1]
-  ini = [0.0, 0.0]
+  noms = [1.0, 1e+1]
+  dens = [1.0, 1e-5]
+  vi = zeros(noms)
+  vo = zeros(dens)[1:end-1]
+  ini = [vo; vi]
 
   td, tr = 0.0, 1.0
-  step(t)  = 0.5 - cospi(clamp((t-td)/tr, 0, 1))/2 
-  dstep(t) = pi/tr*sinpi(clamp((t-td)/tr, 0, 1))/2
-  
-  h = poly2deq([dstep], noms, dens)
+  step(t)   =   0.5    -cospi(clamp((t-td)/tr, 0, 1))/2 
+  dstep(t)  =  pi/tr   *sinpi(clamp((t-td)/tr, 0, 1))/2
+  ddstep(t) = (pi/tr)^2*cospi(clamp((t-td)/tr, 0, 1))/2 # NG 
+  h = poly2deq([dstep, ddstep], noms, dens)
   t, y = ODE.ode23s(h, ini, t)
   y1 = [ a[1] for a in y ]
   y2 = [ a[2] for a in y ]
-  return t, y1, y2
+  y3 = [ a[3] for a in y ]
+  return t, y1, y2, y3
 end
 # ---------------------------------------------------------------------- 
 function run_top()
   t, y1, y2 = lpf1()
-  csvWrite("a.csv", "w", [t y1 y2]')
+  csvWrite("lpf1.csv", "w", [t y1 y2]')
+  t, y1, y2, y3 = hpf1()
+  csvWrite("hpf1.csv", "w", [t y1 y2 y3]')
 end
 # @testset "Foo Tests" begin
 #   @test 1+1 == 2
