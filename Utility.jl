8f93ce6ee53ccaa47172d8045dbf8ad571aee478
7akase <yasuhide.takase@gmail.com>
2016-12-06 00:01:18 +0900

    add function combinator

--- Utility.jl
+++ Utility.jl
@@ -8,7 +8,7 @@ fst(x) = begin a, b = x; return a; end
 snd(x) = begin a, b = x; return b; end
 
 ($)(f :: Function, x) = f(x)
-(*)(g :: Function, f :: Function) = x -> g(f(x))
+(∘)(g :: Function, f :: Function) = x -> g(f(x))
 
 zipWith(f,a,b) = map(f, zip(a,b))
 
