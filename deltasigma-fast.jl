a72b3bbff55a1e46b620a37930d3863e78644634
7akase <yasuhide.takase@gmail.com>
2016-12-14 01:35:15 +0900

    fix bug of ts

--- deltasigma-fast.jl
+++ deltasigma-fast.jl
@@ -45,35 +45,36 @@ Nfft = 2^14
 # ---------------------------------------------------------------------- 
 
 Nwaves = 23
-Nsig = Nfft / Nwaves
 Tfft = Ts * Nfft
+Nsig = Nfft ÷ Nwaves
 Tsig = Tfft / Nwaves
 fsig = 1.0 / Tsig
-
-fin(t) = 0.1 .* sin(2pi*fsig*t)
-stepDac(t) = dDac 
+# 2pi*fsig*A*Ts = dDac  <=>  A = dDac / 2pi fsig Ts
+fin(t) =  (dDac / pi * osr ) .* sinpi(2*fsig*t)
+stepDac(t) = dDac * step_cos(t, 0, Ts*0.005) 
 
 Tsim = Ts * Nfft
 
-ts = Tsig / Nfft * [0:Nfft-1;]
+ts = [0:Nfft-1;] .* Ts
+ts_folded = Tsig / Nfft * [0:Nfft-1;]
 
 # FW-filter response
-t, y0 = ODE.ode23s(G(fin), a0_in, ts)
-y0 = y0[(x -> x == 0).(indexin(t, setdiff(t, ts)))]  # calc all resp
+t, y0 = ODE.ode23s(G(fin), a0_in, ts_folded)
+y0 = y0[(x -> x == 0).(indexin(t, setdiff(t, ts_folded)))]  # calc all resp
 y0 = getindex(y0, [rem(i*Nwaves, Nfft)+1 for i in 0:Nfft-1])  # realloc
 ys0 = [a[1] for a in y0]
 
 # DAC response
 a0_dac = a0_in
-a0_dac[1] = dDac
-t, yDac = ODE.ode23s(G(stepDac), a0_dac, [0.0,Ts])
-yDac = yDac[end][1]
+a0_dac[1] = 1.0 
+t, yDac = ODE.ode23s(G(t -> 0.0), a0_dac, ts)
+yDac = dDac .* yDac[(x -> x == 0).(indexin(t, setdiff(t, ts)))]
+yDac = [a[1] for a in yDac]
 
 # run
-vs = zeros(ys0)
-ys = zeros(ys0)
-y = ys0[1]
-ys[1] = y
+us = fin(ts)
+vs = -1 .* ones(ys0)
+ys = ys0
 prevD = [-1, -1]
 
 print("progress |")
@@ -81,16 +82,19 @@ for i in 2:length(ys0)
   if rem(i, div(length(ys0), 40)) == 0
     print("*")
   end
-  vs[i] = y > 0 ? 1 : -1
-  currentD = runDT(prevD, vs[i])
-  y = y - (currentD[2] - prevD[2])*yDac + ys0[i] - ys0[i-1]
-  ys[i] = y
+  vs[i] = ys[i-1] > 0 ? 1 : -1  # quantizer
+  currentD = runDT(prevD, vs[i])  # Discrete Transfer FunctionA
+  dfb = currentD[2] - prevD[2]
+  ys[i:end] = ys[i:end] - dfb .* yDac[1:length(ys0)-i+1]
+  # y = y - (currentD[2] - prevD[2])*yDac + ys0[i] - ys0[i-1]
 end
 
 hann(n) = [0.5 - 0.5cospi(2x) for x in [0:n-1;]/(n-1)]
 
+psd_u = abs(fft(hann(Nfft) .* us))[1:Nfft÷2]
 psd = abs(fft(hann(Nfft) .* vs))[1:Nfft÷2]
 psd_inband = psd[1:convert(Int, Nfft/2/osr)]
 
-fs = [0:Nfft÷2-1;]/Nfft/Ts
-csvWrite("psd.csv", "w", [fs psd]')
+fs = [1:Nfft÷2;]/Nfft/Ts
+csvWrite("psd.csv", "w", [fs psd psd_u]')
+csvWrite("psd_t.csv", "w", [ts vs us ys yDac]')
