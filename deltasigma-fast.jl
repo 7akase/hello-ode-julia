3d68a249036f152e45eb7c3b92cd8cca1518345a
7akase <yasuhide.takase@gmail.com>
2016-12-14 02:06:01 +0900

    fix bug of tones

--- deltasigma-fast.jl
+++ deltasigma-fast.jl
@@ -59,16 +59,15 @@ ts = [0:Nfft-1;] .* Ts
 ts_folded = Tsig / Nfft * [0:Nfft-1;]
 
 # FW-filter response
-t, y0 = ODE.ode23s(G(fin), a0_in, ts_folded)
-y0 = y0[(x -> x == 0).(indexin(t, setdiff(t, ts_folded)))]  # calc all resp
-y0 = getindex(y0, [rem(i*Nwaves, Nfft)+1 for i in 0:Nfft-1])  # realloc
+t, y0 = ODE.ode23s(G(fin), a0_in, ts)
+y0 = y0[(x -> x == 0).(indexin(t, setdiff(t, ts)))]  # calc all resp
 ys0 = [a[1] for a in y0]
 
 # DAC response
 a0_dac = a0_in
-a0_dac[1] = 1.0 
+a0_dac[1] = dDac 
 t, yDac = ODE.ode23s(G(t -> 0.0), a0_dac, ts)
-yDac = dDac .* yDac[(x -> x == 0).(indexin(t, setdiff(t, ts)))]
+yDac = yDac[(x -> x == 0).(indexin(t, setdiff(t, ts)))]
 yDac = [a[1] for a in yDac]
 
 # run
@@ -94,6 +93,13 @@ hann(n) = [0.5 - 0.5cospi(2x) for x in [0:n-1;]/(n-1)]
 psd_u = abs(fft(hann(Nfft) .* us))[1:Nfft÷2]
 psd = abs(fft(hann(Nfft) .* vs))[1:Nfft÷2]
 psd_inband = psd[1:convert(Int, Nfft/2/osr)]
+
+dc = 3
+sig = [Nwaves-3:Nwaves+3;]
+noi = setdiff([dc:length(psd_inband);], sig)
+s = sum(psd_inband[sig] .^ 2)
+n = sum(psd_inband[noi] .^ 2)
+snr = 10log10(s/n)
 
 fs = [1:Nfft÷2;]/Nfft/Ts
 csvWrite("psd.csv", "w", [fs psd psd_u]')
